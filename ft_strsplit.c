aac114927db7f7fac6a4df14a6a9f990f4a019af
Henri Maronen <hmaronen@c1r5p11.hive.fi>
2021-11-22 09:18:58 +0200

    new

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,8 +6,8 @@
 /*   By: hmaronen <hmaronen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 17:18:05 by hmaronen          #+#    #+#             */
-/*   Updated: 2021/11/18 17:18:15 by hmaronen         ###   ########.fr       */
+/*   Updated: 2021/11/22 09:17:55 by hmaronen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+char	*ft_strtrim(char const *s)
